6b8ee4c2d2e182e8ad89b17ab82e1d3f95a5264d
Haitao <seetide@gmail.com>
2019-08-27 08:26:03 -0400

    create the project with create react app and add homepage with scss file

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      Hellow react
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
