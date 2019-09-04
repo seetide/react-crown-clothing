96022833673ff59de4968c3d3faf5d9a38cadd64
Haitao <seetide@gmail.com>
2019-09-04 15:29:45 -0400

    added redux, and add userActions to header and App component.

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
-import './header.style.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.style.scss';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
-
     <div className='options'>
       <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/contact'>
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>
-          Sign Out{' '}
+          SIGN OUT
         </div>
       ) : (
         <Link className='option' to='/signin'>
-          SIGNIN
+          SIGN IN
         </Link>
       )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
